fe570990ed1e5dde63e949ec0aefe343bcca9439
Alexander Hoppe <thehoppe.a@gmail.com>
2023-04-09 20:07:17 +0100

    Update install.sh

--- software/scripts/install.sh
+++ software/scripts/install.sh
@@ -4,6 +4,8 @@ set echo off
 
 echo "Installing cueServer."
 
+cd cueSystem/software/
+
 if [ "$EUID" -ne 0 ]
   then echo "Please re-run the install scrhipt as root."
   exit
@@ -14,5 +16,7 @@ python3.10 -m pip install --upgrade pip
 apt-get update
 apt install python3 -y
 pip install pyserial websocket-client
+
+python cueSystem/software/cueController.py
 
 exit
